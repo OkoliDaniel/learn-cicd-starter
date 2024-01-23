a61b4e01efdde63cd2b376b05c39b7bab629313c
OkoliDaniel <okolidaniel57@gmail.com>
2024-01-23 16:34:39 +0100

    Add GetAPIKey func unittest, Update ci.yml to run unittest

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -8,7 +8,7 @@ import (
 
 var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
-// GetAPIKey -
+// GetAPIKey - Retrieves the api key from the authorization header
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
